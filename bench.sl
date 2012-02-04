11fd5a9218bafec9b83303daf24f5df720cb1dab
aghiles <aghilesk@gmail.com>
2012-02-04 06:41:33 -0500

    Initial commit for this benchmark.
    - It is the original Dan Maas benhmakr but with shade_mv added
      (no noise call so that we get the VM time mostly) and also
      some fixes for MacOS X in dobench.sh

---
+++ bench.sl
@@ -0,0 +1,118 @@
+/* see readme.txt for copyright info */
+
+/* attempt to stress the shader interpreter
+   this is a modified version of the Mandelbrot fractal
+   algorithm, with noise() called for each iteration
+   as an option. */
+color shader_bench(float u; float v; float call_noise )
+{
+	float i;
+	float x = 0, y = 0;
+
+	float MAXIT = 256; /* max # of iterations */
+
+	for(i = 0; i < MAXIT; i += 1) {
+		float oldx = x, oldy = y;
+		
+		/* Z = Z^2 + C */
+		x = oldx*oldx - oldy*oldy + u;
+		y = 2*oldx*oldy + v;
+
+		/* throw in some noise just for fun */
+		if( call_noise == 1 )
+		{
+			point noiz = point noise(point(10*x, 10*y, 0));
+			x += 0.07*xcomp(noiz);
+			y += 0.07*ycomp(noiz);
+		}
+
+		/* check for escape */
+		float rsquared = x*x + y*y;
+		if(rsquared > 10) {
+			break;
+		}
+	}
+
+	/* output of Mandelbrot iteration */
+	float c = i/MAXIT;
+	return color spline(pow(1-c, 2),
+			    color(0),
+			    color(0),
+			    color(1, 0, 0),
+			    color(0, 0, 1),
+			    color(1),
+			    color(1));
+}
+
+surface bench(
+	string mode = "simple";
+	float samples = 16; /* only used in raytrace modes */
+	float ray_bias = 0.001;
+	float call_noise = 1;)
+{
+	normal NN = normalize(N);
+	
+	Oi = Os;
+	
+	if(mode == "raydiff") {
+		/* diffuse raytracing (ambient occlusion) test */
+		float occ = occlusion(P, NN,
+				      samples,
+				      "adaptive", 1,
+				      "maxerror", 0,
+				      "bias", ray_bias,
+				      "maxdist", 5000);
+		Ci = Oi * (1-occ);
+		
+	} else if(mode == "rayspec") {
+		/* specular raytracing (raytraced reflections) test */
+
+		/* get ray depth */
+		float depth;
+		rayinfo("depth", depth);
+
+		if(depth > 1) {
+			/* don't recurse more than once */
+			Ci = color(0);
+		} else {
+			float maxsamples, minsamples, coneangle;
+			if(depth == 0) {
+				/* initial ray bundle, use normal # of samples */
+				maxsamples = samples;
+				minsamples = samples/4;
+				coneangle = radians(2.5);
+			} else {
+				/* secondary ray shots, use only 1 sample */
+				maxsamples = 1;
+				minsamples = 1;
+				coneangle = 0;
+			}
+			
+			vector II = normalize(I);
+
+			/* shoot the rays */
+			color hitcolor = 0, total = 0;
+			gather("illuminance", P, reflect(II, NN), coneangle, maxsamples,
+			       "distribution", "uniform", "bias", ray_bias,
+			       "surface:Ci", hitcolor) {
+				/* hit */
+				total += hitcolor;
+			} else {
+				/* miss */
+				total += color(1);
+			}
+			total /= maxsamples;
+			
+			Ci = Oi * total;
+		}
+		
+	} else if(mode == "shader") {
+		/* shader interpreter stress test */
+		point oP = transform("object", P);
+		Ci = Oi * shader_bench(xcomp(oP), ycomp(oP), call_noise);
+		
+	} else if(mode == "simple") {
+		/* quick shader for hider test */
+		Ci = Oi * (diffuse(NN) + ambient());
+	}
+}
---
+++ dobench.sh
@@ -0,0 +1,227 @@
+#!/bin/sh
+
+# see readme.txt for copyright info
+
+# CONFIGURATION:
+
+# command for RenderMan shader compiler
+SHADER='shaderdl'
+
+# command for RenderMan renderer
+RENDER='renderdl'
+
+# note: you are responsible for setting $PATH and $RMANTREE
+# appropriately so that 'prman' will work.
+
+# any extra flags you want to pass to prman
+RENDERFLAGS='-progress'
+
+# file for output of benchmark run
+OUTPUT='output.txt'
+
+#####################
+
+# Gracefully exit on Ctrl-C and remove all temporary files
+trap 'echo "Aborted." ; rm -f TEMP TIME SYSTEM_PROFILE; exit 1' TERM INT
+
+# start time (for reporting use only, not for timing)
+DATE=`date`
+
+# determine operating system and # of CPUs
+PLATFORM=`uname`
+
+if [ "$PLATFORM" = "Linux" ]; then
+    echo "Detected OS: Linux"
+    CPUS=`grep processor /proc/cpuinfo | wc -l | sed 's/^[ \t]*//'`
+elif [ "$PLATFORM" = "Darwin" ]; then
+    echo "Detected OS: Mac OSX"
+	CPUS=`sysctl -n machdep.cpu.core_count`
+else
+    echo "This script is not compatible with this OS: $PLATFORM"
+    echo "Compile bench.sl and manually render each RIB file (except geom.rib)."
+    echo "For timing, uncomment the \"Option\" line at the top of each RIB."
+    exit 1
+fi
+
+if [ $CPUS -eq 1 ]; then
+    echo "Detected 1 CPU"
+elif [ $CPUS -gt 1 ]; then
+    echo "Detected $CPUS CPUs"
+fi
+
+# test for presence of /usr/bin/time
+if [ ! -x /usr/bin/time ]; then
+    echo "/usr/bin/time is not available - please install it."
+    echo "(on RedHat, install the 'time' RPM, on SuSE, install 'util-linux')"
+    echo "(on Debian, install the 'time' package)"
+    exit 1
+fi
+
+# compile the shader
+echo "Compiling shader..."
+if ! "$SHADER" bench.sl; then
+    echo "Error compiling shader bench.sl"
+    exit 1
+fi
+
+# usage:
+# timerender "shader" foo.rib 2
+# arg1: user-visible name of this test
+# arg2: RIB file to render
+# arg3: # of processors to use (1 or 2)
+# puts elapsed wall-clock time in seconds in a file called TIME
+
+function timerender {
+    echo "Running $1 benchmark "
+  
+    if [ $3 -eq 1 ]; then
+	echo "(one thread)..."
+    elif [ $3 -gt 1 ]; then
+	echo "(multithreading)..."
+    else
+	echo "# of threads must be 1 or more (was $3)"
+	exit 1
+    fi
+
+    # run renderer
+    if [ "$PLATFORM" = "Linux" ]; then
+	/usr/bin/time -f %e -o TIME "$RENDER" $RENDERFLAGS -p:$3 $2
+    elif [ "$PLATFORM" = "Darwin" ]; then
+	# Mac OSX's time(1) command has no ability to output to a file
+	# so, redirect stderr to TEMP, then strip off everything but
+	# the wall clock time in seconds. We must run the renderer
+	# in a subshell in order to send its stderr to the console rather
+	# than the TEMP file.
+	/usr/bin/time sh -c "'$RENDER' $RENDERFLAGS -p:$3 $2 2>&1 " 2>TEMP && \
+	(sed 's/ real.*//' | sed 's/^[ \t]*//') < TEMP > TIME && rm TEMP
+    fi
+
+    # check return value from the render
+    if [ ! $? ]; then
+	echo "Error running $1 benchmark"
+	exit 1
+    fi
+    local RESULT=`cat TIME`
+    echo "$1 benchmark done: $RESULT sec"
+}
+
+echo "Running single thread benchmarks..."
+timerender "shader" shader.rib 1
+SHADERTIME=`cat TIME`
+
+timerender "shader_vm" shader_vm.rib 1
+SHADERVMTIME=`cat TIME`
+
+timerender "hider" hider.rib 1
+HIDERTIME=`cat TIME`
+
+timerender "diffuse raytrace" raydiff.rib 1
+RAYDIFFTIME=`cat TIME`
+
+timerender "specular raytrace" rayspec.rib 1
+RAYSPECTIME=`cat TIME`
+
+if [ $CPUS -gt 1 ]; then
+    echo "Running multithread benchmarks..."
+    timerender "shader" shader.rib $CPUS
+    DUALSHADERTIME=`cat TIME`
+
+    timerender "shader_vm" shader_vm.rib $CPUS
+    DUALSHADERVMTIME=`cat TIME`
+
+    timerender "hider" hider.rib $CPUS
+    DUALHIDERTIME=`cat TIME`
+
+    timerender "diffuse raytrace" raydiff.rib $CPUS
+    DUALRAYDIFFTIME=`cat TIME`
+
+    timerender "specular raytrace" rayspec.rib $CPUS
+    DUALRAYSPECTIME=`cat TIME`
+fi
+
+echo "All benchmarks done."
+
+# print output
+
+cat > "$OUTPUT" <<EOF
+RenderMan Benchmark v3
+by Maas Digital LLC
+(11 November 2004)
+
+Run on $HOSTNAME at $DATE
+Render command was $RENDER $RENDERFLAGS
+EOF
+
+# print renderer version
+$RENDER -version >> "$OUTPUT" 2>&1
+echo >> "$OUTPUT" # add a newline
+
+# determine system info
+if [ "$PLATFORM" = "Linux" ]; then
+    # Linux
+    echo "Gathering system info..."
+    OS=`uname -s -r`
+    NCPUS=`grep '^processor' /proc/cpuinfo | wc -l | sed 's/^[ \t]*//'`
+    CPUTYPE=`grep model\ name /proc/cpuinfo  | head -n 1 | sed 's/.*: //'`
+    CPUSPD=`grep cpu\ MHz /proc/cpuinfo  | head -n 1 | sed 's/.*: //'`
+    CPUCACHE=`grep cache\ size /proc/cpuinfo | head -n 1 | sed 's/.*: //'`
+    cat >> "$OUTPUT" <<EOF
+System Info
+-----------
+OS:          $OS
+# of CPUs:   $NCPUS (including HyperThreading virtual CPUs)
+CPU type:    $CPUTYPE
+CPU speed:   $CPUSPD MHz
+CPU cache:   $CPUCACHE
+EOF
+elif [ "$PLATFORM" = "Darwin" ]; then
+    # Mac OSX (use the file generated above with system_profiler)
+    cat SYSTEM_PROFILE | head -n 12 >> "$OUTPUT"
+fi
+
+cat >> "$OUTPUT" <<EOF
+
+Single thread Benchmarks
+------------------------
+Shader time:             $SHADERTIME sec
+Shader VM time:          $SHADERVMTIME sec
+Hider time:              $HIDERTIME sec
+Diffuse raytrace time:   $RAYDIFFTIME sec
+Specular raytrace time:  $RAYSPECTIME sec
+
+EOF
+
+if [ $CPUS -gt 1 ]; then
+    cat >> "$OUTPUT" <<EOF
+Multithread Benchmarks
+----------------------
+Shader time:             $DUALSHADERTIME sec
+Shader VM time:          $DUALSHADERVMTIME sec
+Hider time:              $DUALHIDERTIME sec
+Diffuse raytrace time:   $DUALRAYDIFFTIME sec
+Specular raytrace time:  $DUALRAYSPECTIME sec
+
+EOF
+fi
+
+# append checksums of various files, to make sure
+# they were not modified
+echo "Checksums:" >> "$OUTPUT"
+
+function do_md5sum {
+    if [ "$PLATFORM" = "Linux" ]; then
+        # Linux
+	md5sum "$@"
+    elif [ "$PLATFORM" = "Darwin" ]; then
+       # Mac OSX (munge md5 output to look like md5sum)
+	md5 "$@" | sed 's#MD5 (\(.*\)) = \(.*\)#\2  \1#'
+    fi
+}
+
+do_md5sum bench.sl shader.rib raydiff.rib rayspec.rib hider.rib geom.rib >> $OUTPUT
+do_md5sum $OUTPUT >> $OUTPUT
+
+# clean up
+rm -f TEMP TIME SYSTEM_PROFILE
+
+echo "Done. See results in $OUTPUT."
